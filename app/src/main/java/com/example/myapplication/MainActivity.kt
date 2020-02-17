2f3144dd3b1fd53fa3baf66632245027664d1995
Hendra Nicholas <hendra@41studio.com>
2020-02-18 01:27:17 +0700

    implement basic CRUD API

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,23 +22,78 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val client = OkHttpClient().newBuilder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+            })
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
+
         val gson = GsonBuilder()
             .setLenient()
             .create()
 
         val service = Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
             .create(SampleService::class.java)
 
+        // Get all books
+        // GET /books.json
         service.getBooks()
             .enqueue(object : Callback<List<Book>> {
                 override fun onResponse(call: Call<List<Book>>, response: Response<List<Book>>) {
-                    response.body()?.forEach { println("TAG_: $it") }
+                    response.body()?.forEach { println("TAG_: ${it}") }
                 }
 
                 override fun onFailure(call: Call<List<Book>>, t: Throwable) = t.printStackTrace()
             })
+
+        // Get a book by id
+        // GET /books/1.json
+//        service.getBook(2)
+//            .enqueue(object : Callback<Book> {
+//                override fun onResponse(call: Call<Book>, response: Response<Book>) {
+//                    println("TAG_: ${response.body()}")
+//                }
+//
+//                override fun onFailure(call: Call<Book>, t: Throwable) = t.printStackTrace()
+//            })
+
+        // Create book
+        // GET /books.json
+//        service.addBook("Title 1", "Author 1")
+//            .enqueue(object : Callback<Book> {
+//                override fun onResponse(call: Call<Book>, response: Response<Book>) {
+//                    println("TAG_: ${response.body()}")
+//                }
+//
+//                override fun onFailure(call: Call<Book>, t: Throwable) = t.printStackTrace()
+//            })
+
+        // Update book
+        // PATCH /books.json
+//        service.updateBook(17,"Title 3", "Author 1")
+//            .enqueue(object : Callback<Book> {
+//                override fun onResponse(call: Call<Book>, response: Response<Book>) {
+//                    println("TAG_: ${response.body()}")
+//                }
+//
+//                override fun onFailure(call: Call<Book>, t: Throwable) = t.printStackTrace()
+//            })
+
+        // Delete book
+        // DELETE /books.json
+//        service.deleteBook(2)
+//            .enqueue(object : Callback<Book> {
+//                override fun onResponse(call: Call<Book>, response: Response<Book>) {
+//                    println("TAG_: ${response.code()}")
+//                }
+//
+//                override fun onFailure(call: Call<Book>, t: Throwable) = t.printStackTrace()
+//            })
     }
 }
--- app/src/main/java/com/example/myapplication/SampleService.kt
+++ app/src/main/java/com/example/myapplication/SampleService.kt
@@ -1,10 +1,35 @@
 package com.example.myapplication
 
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Headers
+import retrofit2.http.*
 
 interface SampleService {
+    // GET /books
     @GET("books.json")
     fun getBooks(): Call<List<Book>>
+
+    // GET /books/{id}
+    @GET("books/{id}.json")
+    fun getBook(@Path("id") id:Int): Call<Book>
+
+    // POST /books
+    @FormUrlEncoded
+    @POST("books.json")
+    fun addBook(
+        @Field("book[title]") title: String?,
+        @Field("book[author]") body: String?
+    ): Call<Book>
+
+    // PATCH /books
+    @FormUrlEncoded
+    @PATCH("books/{id}.json")
+    fun updateBook(
+        @Path("id") id:Int,
+        @Field("book[title]") title: String?,
+        @Field("book[author]") body: String?
+    ): Call<Book>
+
+    // DELETE /books/{id}
+    @DELETE("books/{id}.json")
+    fun deleteBook(@Path("id") id:Int): Call<Book>
 }
